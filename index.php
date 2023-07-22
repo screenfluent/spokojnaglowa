9e6a694f0a6c37736a43dd193cc28d0f65f1f822
Szymon Rączka <116732045+screenfluent@users.noreply.github.com>
2023-07-22 18:58:30 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
 
-<main>
+<article>
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
         <?php the_excerpt(); ?>
     <?php endwhile; endif; ?>
-</main>
+</article>
 
 <?php get_footer(); ?>
