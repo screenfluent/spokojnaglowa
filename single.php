818eeb546bfe483b42e5fd24068a25c14ddb6d90
Szymon Rączka <116732045+screenfluent@users.noreply.github.com>
2023-07-22 18:58:29 +0200

    Update single.php

--- single.php
+++ single.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
 
-<main>
+<article>
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
         <h1><?php the_title(); ?></h1>
         <?php the_content(); ?>
     <?php endwhile; endif; ?>
-</main>
+</article>
 
 <?php get_footer(); ?>
