88a00eb7d361d527bd4947e81bf4431047696deb
Szymon Rączka <116732045+screenfluent@users.noreply.github.com>
2023-07-22 17:49:46 +0200

    Update header.php

--- header.php
+++ header.php
@@ -1,7 +1,6 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-    <title><?php echo esc_html( get_bloginfo( 'name' ) ); ?></title>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php wp_head(); ?>
