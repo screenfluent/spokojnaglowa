d5dd60a3cf3c5bef2cc672fd2427d8f36e3befa8
Szymon Rączka <116732045+screenfluent@users.noreply.github.com>
2023-07-22 18:58:32 +0200

    Update header.php

--- header.php
+++ header.php
@@ -6,3 +6,13 @@
     <?php wp_head(); ?>
 </head>
 <body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="<?php echo esc_url(home_url('/')); ?>">Home</a></li>
+                <li><a href="<?php echo esc_url(home_url('/about')); ?>">About</a></li>
+                <li><a href="<?php echo esc_url(home_url('/contact')); ?>">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+    <main>
