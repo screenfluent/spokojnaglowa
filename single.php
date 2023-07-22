7dc1c7b416fdaee9e5973da319e73668b63f1656
Szymon Rączka <116732045+screenfluent@users.noreply.github.com>
2023-07-22 20:24:21 +0200

    Update single.php

--- single.php
+++ single.php
@@ -1,7 +1,5 @@
 <?php get_header(); ?>
 
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-
 <?php generate_breadcrumb_structured_data(); ?>
 
 <nav aria-label="breadcrumb">
@@ -14,10 +12,37 @@
 </nav>
 
 <article>
-    <h1><?php the_title(); ?></h1>
-    <?php the_content(); ?>
+    <header>
+        <h1><?php the_title(); ?></h1>
+        <p>Tags: 
+            <?php
+            $posttags = get_the_tags();
+            if ($posttags) {
+                foreach($posttags as $tag) {
+                    echo '<a href="'.get_tag_link($tag->term_id).'">'.$tag->name.'</a>, '; 
+                }
+            }
+            ?>
+        </p>
+    </header>
+
+    <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+        <section id="intro">
+            <h2>Introduction</h2>
+            <?php the_content(); ?>
+        </section>
+    <?php endwhile; endif; ?>
+
+    <!-- Add more sections as needed -->
+
+    <footer>
+        <!-- Add footer content as needed -->
+    </footer>
 </article>
 
-<?php endwhile; endif; ?>
+<aside>
+    <h3>About the Author</h3>
+    <p><?php the_author_meta('description'); ?></p>
+</aside>
 
 <?php get_footer(); ?>
